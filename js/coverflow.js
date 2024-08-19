52fc24615c32f3be877dad4c310eff7a48d5172d
Percival Aceron <perci.aceron@gmail.com>
2024-08-19 20:17:49 +0800

    work in action section (#8)

---
+++ js/coverflow.js
@@ -0,0 +1,17 @@
+const swiper = new Swiper(".coverflow", {
+  loop: true,
+  slidesPerView: 3,
+  centeredSlides: true,
+  effect: "coverflow",
+  coverflowEffect: {
+    stretch: 0,
+    depth: 300,
+    modifier: 1,
+    rotate: 0,
+    slideShadows: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
