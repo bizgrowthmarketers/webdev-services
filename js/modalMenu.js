539ccb635cc6fd2901144da163457ffe0299f2eb
Biz Growth Marketers <business.growthmarketers@gmail.com>
2024-08-26 23:03:56 -0700

    hamburger menu, section titles (#30)
    Co-authored-by: Percival Aceron <perci.aceron@gmail.com>

--- js/modalMenu.js
+++ js/modalMenu.js
@@ -1,19 +1,8 @@
 const hamburger = document.getElementById("hamburger");
 
+
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
+  document.getElementById("nav").classList.toggle("active");
 });
 
-const modal = document.getElementById("modal");
-const toggleButton = document.getElementById("hamburger");
-
-toggleButton.addEventListener("click", () => {
-  modal.style.display = modal.style.display === "block" ? "none" : "block";
-});
-
-window.onclick = function (event) {
-  if (event.target === modal) {
-    modal.style.display = "none";
-    hamburger.classList.remove("active");
-  }
-};
