41e9ce2bd450c77bc428d8b67f5470acb060c5b6
Percival Aceron <perci.aceron@gmail.com>
2024-08-28 14:22:27 +0800

    button links, counter duration

--- js/animateCounter.js
+++ js/animateCounter.js
@@ -7,21 +7,19 @@ function animateCounter(element, start, end, duration) {
     if (progress < 1) {
       window.requestAnimationFrame(step);
     } else {
-      element.textContent += "+"; // Add the "+" sign when the animation is complete
+      element.textContent += "+";
 
-      // Restart the counter after 1 second
       setTimeout(() => {
-        element.textContent = "0"; // Reset the counter to 0
-        animateCounter(element, 0, end, duration); // Start the animation again
-      }, 2000); // 1 second delay before restarting
+        element.textContent = "0";
+        animateCounter(element, 0, end, duration);
+      }, 5000);
     }
   };
   window.requestAnimationFrame(step);
 }
 
-// Select all elements with the counter-container class
 const counters = document.querySelectorAll(".counter-container");
 counters.forEach((counter) => {
   const endValue = parseInt(counter.textContent.replace(/\D/g, ""), 10);
-  animateCounter(counter, 0, endValue, 15500);
+  animateCounter(counter, 0, endValue, 3000);
 });
