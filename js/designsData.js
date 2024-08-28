f2493c5d165c65bc7a50ea3a4bfa4f90bf23877f
Percival Aceron <perci.aceron@gmail.com>
2024-08-28 10:45:19 +0800

    portfolio edit
    update , git, pdf links and screenshot, plugin update

--- js/designsData.js
+++ js/designsData.js
@@ -1,33 +1,39 @@
 // designsData.js
 const designsData = [
   {
-    imgSrc: "./images/cocktailsapp.jpg",
-    altText: "cocktailsapp website",
-    linkUrl: "https://bizgrowthmarketers.tech/cocktails-app/",
+    imgSrc: "./images/webDesigns/Constra.webp",
+    altText: "a construction website",
+    linkUrl:
+      "https://drive.google.com/file/d/1KyLjYumPgz7TO8LcMS4JRRNsfAwMSm6n/view",
   },
   {
-    imgSrc: "./images/filmoteka.jpg",
-    altText: "filmoteka website",
-    linkUrl: "https://bizgrowthmarketers.tech/movie-app/",
+    imgSrc: "./images/webDesigns/HellEn.webp",
+    altText: "english tutorial website",
+    linkUrl:
+      "https://drive.google.com/file/d/1TN3zz78Gxzc2mCwfEBfdgerPOjMe9Pb5/view",
   },
   {
-    imgSrc: "./images/foodgram.jpg",
-    altText: "foodgram website",
-    linkUrl: "https://bizgrowthmarketers.tech/food-gram/",
+    imgSrc: "../images/webDesigns/FoodSta.webp",
+    altText: "a foodsta website",
+    linkUrl:
+      "https://drive.google.com/file/d/1UcBPf9Vjawtz1M8sx6Bwg0mEctRnpOOY/view",
   },
   {
-    imgSrc: "./images/icecreamapp.jpg",
-    altText: "icecreamapp website",
-    linkUrl: "https://bizgrowthmarketers.tech/ice-cream-app/",
+    imgSrc: "./images/webDesigns/YatchJet.webp",
+    altText: "yatch boat rental website",
+    linkUrl:
+      "https://drive.google.com/file/d/1MO8mfje-o9ob1Ie9Gil4nsPRbkfDs1Ox/view",
   },
   {
-    imgSrc: "./images/realstatehomyz.jpg",
-    altText: "realstatehomyz website",
-    linkUrl: "https://bizgrowthmarketers.tech/real-estate-homyz/",
+    imgSrc: "./images/webDesigns/GymFit.webp",
+    altText: "gym website",
+    linkUrl:
+      "https://drive.google.com/file/d/1Uh65XH5pY4Z7_43FoK63eLuFlEc5TNug/view",
   },
   {
-    imgSrc: "./images/watchspot.jpg",
-    altText: "watchspot website",
-    linkUrl: "https://bizgrowthmarketers.tech/the-watch-spot/",
+    imgSrc: "./images/webDesigns/Restaurant.webp",
+    altText: "restaurant website",
+    linkUrl:
+      "https://drive.google.com/file/d/1qGroA5aGcJkviAggCemIxO_6UsGPCbky/view",
   },
 ];
--- js/modalMenu.js
+++ js/modalMenu.js
@@ -1,6 +1,5 @@
 const hamburger = document.getElementById("hamburger");
 
-
 hamburger.addEventListener("click", function () {
   hamburger.classList.toggle("active");
   document.getElementById("nav").classList.toggle("active");
