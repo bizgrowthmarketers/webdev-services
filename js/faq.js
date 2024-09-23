b0abfa891efb6bc04b9aa935aefd8e301fe2d169
Judielyn Pagente <137527318+jpagente@users.noreply.github.com>
2024-09-24 07:51:51 +0800

    Faq update for responsiveness (#52)
    Faq update for responsiveness

--- js/faq.js
+++ js/faq.js
@@ -1,10 +1,16 @@
 // Select all accordion toggle buttons
 document.querySelectorAll('.accordion-toggle').forEach((button) => {
-    button.addEventListener('click', () => {
-      const accordionItem = button.parentElement;
-      const isActive = accordionItem.classList.contains('active');
-  
-      // Toggle the active class
-      accordionItem.classList.toggle('active');
-    });
+  button.addEventListener('click', () => {
+    const accordionItem = button.parentElement;
+    const accordionAnswer = button.nextElementSibling;
+    const isActive = accordionItem.classList.contains('active');
+    // Toggle the active class
+    accordionItem.classList.toggle('active');
+    // Adjust the height of the accordion answer
+    if (isActive) {
+      accordionAnswer.style.maxHeight = null;
+    } else {
+      accordionAnswer.style.maxHeight = accordionAnswer.scrollHeight + "px";
+    }
   });
+});
